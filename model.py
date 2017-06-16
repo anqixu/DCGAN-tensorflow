d05602ba583a6237c012e5987e0a96d54c0171f8
Anqi Xu <ax@elementai.com>
2017-06-16 17:34:10 +0000

    added 3-layer architecture variant (changeable via Model ctor)

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ class DCGAN(object):
          batch_size=64, sample_num = 64, output_height=64, output_width=64,
          z_dim=100, gf_dim=64, df_dim=64,
          gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_name='default',
-         input_fname_pattern='*.jpg', checkpoint_dir=None, sample_dir=None):
+         input_fname_pattern='*.jpg', checkpoint_dir=None, sample_dir=None, three_layers=False):
     """
 
     Args:
@@ -31,6 +31,14 @@ class DCGAN(object):
       dfc_dim: (optional) Dimension of discrim units for fully connected layer. [1024]
       c_dim: (optional) Dimension of image color. For grayscale input, set to 1. [3]
     """
+    self.generator = self.generator_4layers
+    self.discriminator = self.discriminator_4layers
+    self.sampler = self.sampler_4layers
+    if three_layers:
+      self.generator = self.generator_3layers
+      self.discriminator = self.discriminator_3layers
+      self.sampler = self.sampler_3layers
+
     self.sess = sess
     self.crop = crop
 
@@ -297,7 +305,7 @@ class DCGAN(object):
         if np.mod(counter, 500) == 2:
           self.save(config.checkpoint_dir, counter)
 
-  def discriminator(self, image, y=None, reuse=False):
+  def discriminator_4layers(self, image, y=None, reuse=False):
     with tf.variable_scope("discriminator") as scope:
       if reuse:
         scope.reuse_variables()
@@ -310,7 +318,19 @@ class DCGAN(object):
 
       return tf.nn.sigmoid(h4), h4
 
-  def generator(self, z, y=None):
+  def discriminator_3layers(self, image, y=None, reuse=False):
+    with tf.variable_scope("discriminator") as scope:
+      if reuse:
+        scope.reuse_variables()
+
+      h0 = lrelu(conv2d(image, self.df_dim, name='d_h0_conv'))
+      h1 = lrelu(self.d_bn1(conv2d(h0, self.df_dim*2, name='d_h1_conv')))
+      h2 = lrelu(self.d_bn2(conv2d(h1, self.df_dim*4, name='d_h2_conv')))
+      h3 = linear(tf.reshape(h2, [self.batch_size, -1]), 1, 'd_h2_lin')
+
+      return tf.nn.sigmoid(h3), h3
+
+  def generator_4layers(self, z, y=None):
     with tf.variable_scope("generator") as scope:
       s_h, s_w = self.output_height, self.output_width
       s_h2, s_w2 = conv_out_size_same(s_h, 2), conv_out_size_same(s_w, 2)
@@ -343,7 +363,35 @@ class DCGAN(object):
 
       return tf.nn.tanh(h4)
 
-  def sampler(self, z, y=None):
+  def generator_3layers(self, z, y=None):
+    with tf.variable_scope("generator") as scope:
+      s_h, s_w = self.output_height, self.output_width
+      s_h2, s_w2 = conv_out_size_same(s_h, 2), conv_out_size_same(s_w, 2)
+      s_h4, s_w4 = conv_out_size_same(s_h2, 2), conv_out_size_same(s_w2, 2)
+      s_h8, s_w8 = conv_out_size_same(s_h4, 2), conv_out_size_same(s_w4, 2)
+
+      # project `z` and reshape
+      self.z_, self.h0_w, self.h0_b = linear(
+          z, self.gf_dim*4*s_h8*s_w8, 'g_h0_lin', with_w=True)
+
+      self.h0 = tf.reshape(
+          self.z_, [-1, s_h8, s_w8, self.gf_dim * 4])
+      h0 = tf.nn.relu(self.g_bn0(self.h0))
+
+      self.h1, self.h1_w, self.h1_b = deconv2d(
+          h0, [self.batch_size, s_h4, s_w4, self.gf_dim*2], name='g_h1', with_w=True)
+      h1 = tf.nn.relu(self.g_bn1(self.h1))
+
+      h2, self.h2_w, self.h2_b = deconv2d(
+          h1, [self.batch_size, s_h2, s_w2, self.gf_dim*1], name='g_h2', with_w=True)
+      h2 = tf.nn.relu(self.g_bn2(h2))
+
+      h3, self.h4_w, self.h4_b = deconv2d(
+          h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3', with_w=True)
+
+      return tf.nn.tanh(h3)
+
+  def sampler_4layers(self, z, y=None):
     with tf.variable_scope("generator") as scope:
       scope.reuse_variables()
 
@@ -371,6 +419,32 @@ class DCGAN(object):
       h4 = deconv2d(h3, [self.batch_size, s_h, s_w, self.c_dim], name='g_h4')
 
       return tf.nn.tanh(h4)
+ 
+
+  def sampler_3layers(self, z, y=None):
+    with tf.variable_scope("generator") as scope:
+      scope.reuse_variables()
+
+      s_h, s_w = self.output_height, self.output_width
+      s_h2, s_w2 = conv_out_size_same(s_h, 2), conv_out_size_same(s_w, 2)
+      s_h4, s_w4 = conv_out_size_same(s_h2, 2), conv_out_size_same(s_w2, 2)
+      s_h8, s_w8 = conv_out_size_same(s_h4, 2), conv_out_size_same(s_w4, 2)
+
+      # project `z` and reshape
+      h0 = tf.reshape(
+          linear(z, self.gf_dim*4*s_h8*s_w8, 'g_h0_lin'),
+          [-1, s_h8, s_w8, self.gf_dim * 4])
+      h0 = tf.nn.relu(self.g_bn0(h0, train=False))
+
+      h1 = deconv2d(h0, [self.batch_size, s_h4, s_w4, self.gf_dim*2], name='g_h1')
+      h1 = tf.nn.relu(self.g_bn1(h1, train=False))
+
+      h2 = deconv2d(h1, [self.batch_size, s_h2, s_w2, self.gf_dim*1], name='g_h2')
+      h2 = tf.nn.relu(self.g_bn2(h2, train=False))
+
+      h3 = deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3')
+
+      return tf.nn.tanh(h3)
  
   def load_mnist(self):
     data_dir = os.path.join("./data", self.dataset_name)
